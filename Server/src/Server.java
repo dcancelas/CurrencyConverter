c776d828609d95ada90858c8b5c48bcaa166c8d3
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2021-03-04 20:25:22 +0100

    Some changes

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,11 +1,15 @@
-import java.io.IOException;
+import org.ini4j.Wini;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
+    static int port;
+
     public static void main(String[] args) throws IOException {
-        int port = 4242;
+        readConfigFile();
         System.out.println("Iniciando servidor en el puerto " + port);
         ServerSocket serverSocket = new ServerSocket(port);
 
@@ -18,5 +22,12 @@ public class Server {
             Thread t = new ClientHandler(s);
             t.start();
         }
+    }
+
+    public static void readConfigFile() throws IOException {
+        File file = new File("./server.ini");
+        file.createNewFile();
+        Wini ini = new Wini(file);
+        port = ini.get("server", "port", int.class);
     }
 }
